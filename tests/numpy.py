ce4a13697171e00cad436c32de350932d8d99757
Dorian Birraux <dorianb@wolfram.com>
2018-02-23 10:18:19 +0100

    Add some numpy unit tests

---
+++ tests/numpy.py
@@ -0,0 +1,94 @@
+import unittest
+import numpy
+
+from wxfserializer.wxfexprprovidernumpy import WXFExprProviderNumPy
+from wxfserializer.wxfexprprovider import WXFExprProvider
+from wxfserializer.wxfdataconsumer import WXFDataConsumer, InMemoryWXFDataConsumer
+from wxfserializer.serializer import WXFExprSerializer
+
+
+
+class TestNumpySerialization(unittest.TestCase):
+    @staticmethod
+    def initDefault():
+        return TestNumpySerialization.init(True, False)
+
+    @staticmethod
+    def initBothArraySupport():
+        return TestNumpySerialization.init(True, True)
+
+    @staticmethod
+    def initOnlyRA():
+        return TestNumpySerialization.init(False, True)
+
+
+    @staticmethod
+    def init(pa, ra):
+        expr_provider = WXFExprProviderNumPy(packed_array_support=pa, rawarray_support=ra)
+        data_consumer = InMemoryWXFDataConsumer()
+        serializer = WXFExprSerializer(expr_provider, data_consumer)
+        return (serializer, data_consumer)
+
+    def test_dimensions(self):
+        provider = WXFExprProviderNumPy()
+        arr = numpy.ndarray([2,1,3])
+        wxfExpr = next(provider.pythonToWXFExpr(arr))
+        self.assertEqual(wxfExpr.dimensions, (2, 1, 3) )
+
+    def test_zero_dimension(self):
+        provider = WXFExprProviderNumPy()
+        arr = numpy.ndarray([2, 0, 3])
+        with self.assertRaises(Exception) as err:
+            next(provider.pythonToWXFExpr(arr))
+
+        self.assertEqual(str(err.exception), "Dimensions must be positive integers.")
+
+    def test_int8_PA(self):
+        s, d = TestNumpySerialization.initDefault()
+        arr = numpy.array([[-(1 << 7), -1], [1, (1 << 7) - 1]], numpy.int8)
+        s.serialize(arr)
+        self.assertEqual(
+            d.data(), b'\x38\x3a\xc1\x00\x02\x02\x02\x80\xff\x01\x7f')
+
+    def test_int8_Both(self):
+        s, d = TestNumpySerialization.initBothArraySupport()
+        arr = numpy.array([[-(1 << 7), -1], [1, (1 << 7) - 1]], numpy.int8)
+        s.serialize(arr)
+        self.assertEqual(
+            d.data(), b'\x38\x3a\xc1\x00\x02\x02\x02\x80\xff\x01\x7f')
+
+    def test_int8_RA(self):
+        s, d = TestNumpySerialization.initOnlyRA()
+        arr = numpy.array([[-(1 << 7), -1], [1, (1 << 7) - 1]], numpy.int8)
+        s.serialize(arr)
+        self.assertEqual(
+            d.data(), b'\x38\x3a\xc2\x00\x02\x02\x02\x80\xff\x01\x7f')
+
+    def test_uint8_PA(self):
+        s, d = TestNumpySerialization.initDefault()
+        arr = numpy.array([[0,(1 << 7)]], numpy.uint8)
+        s.serialize(arr)
+        self.assertEqual(
+            d.data(), b'\x38\x3a\xc1\x01\x02\x01\x02\x00\x00\x80\x00')
+
+    def test_uint8_RA(self):
+        s, d = TestNumpySerialization.initBothArraySupport()
+        arr = numpy.array([[0, (1 << 7)]], numpy.uint8)
+        s.serialize(arr)
+        self.assertEqual(
+            d.data(), b'\x38\x3a\xc2\x10\x02\x01\x02\x00\x80')
+    
+    # def test_isupper(self):
+    #     self.assertTrue('FOO'.isupper())
+    #     self.assertFalse('Foo'.isupper())
+
+    # def test_split(self):
+    #     s = 'hello world'
+    #     self.assertEqual(s.split(), ['hello', 'world'])
+    #     # check that s.split fails when the separator is not a string
+    #     with self.assertRaises(TypeError):
+    #         s.split(2)
+
+
+if __name__ == '__main__':
+    unittest.main()
