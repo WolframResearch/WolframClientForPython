0ba516b0fb6f4b30a6c6361c013593f062924393
riccardodivirgilio <riccardod@wolfram.com>
2018-03-19 14:45:09 +0100

    cleaning up formatting

--- wolframclient/cli/commands/serialize.py
+++ wolframclient/cli/commands/serialize.py
@@ -3,7 +3,6 @@
 from __future__ import absolute_import, print_function, unicode_literals
 
 from wolframclient.cli.utils import SimpleCommand
-from wolframclient.language.expression import wl
 from wolframclient.serializers import export, available_formats, DEFAULT_FORMAT
 from wolframclient.utils.encoding import force_text
 
@@ -12,7 +11,12 @@ import decimal
 class Command(SimpleCommand):
 
     def add_arguments(self, parser):
-        parser.add_argument('--format', dest = 'format', choices=tuple(available_formats.keys()), default = DEFAULT_FORMAT)
+        parser.add_argument(
+            '--format', 
+            dest = 'format', 
+            choices=tuple(available_formats.keys()), 
+            default = DEFAULT_FORMAT
+        )
 
-    def handle(self, format):
+    def handle(self, format = DEFAULT_FORMAT):
         self.print(force_text(export(decimal.Decimal('1.2'), format = format)))
