e707c97dc01a6896f73db3e231e999cb9b263428
riccardodivirgilio <riccardod@wolfram.com>
2018-03-20 16:00:32 +0100

    WolframLanguageExceptionBase should inherit from Exception, adding hooks to remove the traceback and add a failure tag

--- wolframclient/language/exceptions.py
+++ wolframclient/language/exceptions.py
@@ -7,7 +7,7 @@ from wolframclient.serializers.serializable import WLSerializable
 from wolframclient.utils.decorators import to_dict
 from wolframclient.utils.encoding import safe_force_text
 
-class WolframLanguageExceptionBase(WLSerializable, object):
+class WolframLanguageExceptionBase(WLSerializable, Exception):
 
     def __init__(self):
         raise NotImplementedError
@@ -33,7 +33,7 @@ class WolframLanguageExceptionBase(WLSerializable, object):
             ))
         )
 
-    def add_traceback(self):
+    def show_traceback(self):
         return True
 
     @to_dict
@@ -47,7 +47,7 @@ class WolframLanguageExceptionBase(WLSerializable, object):
         if parameters:
             yield "MessageParameters", parameters
 
-        if self.add_traceback() and self.tb:
+        if self.show_traceback() and self.tb:
 
             from wolframclient.language.traceback import serialize_traceback
 
@@ -55,16 +55,23 @@ class WolframLanguageExceptionBase(WLSerializable, object):
 
 class WolframLanguageExceptionFromPython(WolframLanguageExceptionBase):
 
-    def __init__(self, exception, exec_info = None):
-        self.original_exception = exception
+    def __init__(self, exception, exec_info = None, show_traceback = True, failure_tag = None):
+
+        self.exception = exception
+
+        self._show_traceback   = show_traceback
+        self._failure_tag      = failure_tag
 
         if exec_info:
             self.set_traceback(*exec_info)
         else:
             self.set_traceback(None, None, None)
 
+    def show_traceback(self):
+        return bool(self._show_traceback)
+
     def failure_tag(self):
-        return safe_force_text(self.original_exception.__class__.__name__)
+        return safe_force_text(self._failure_tag or self.exception.__class__.__name__)
 
     def failure_template(self):
-        return safe_force_text(self.original_exception)
+        return safe_force_text(self.exception)
