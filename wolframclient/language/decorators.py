51cdcaa11e34faf50fe60a446122783081ab158b
riccardodivirgilio <riccardod@wolfram.com>
2018-03-22 17:10:36 +0100

    custom subclass and custom wrappers might fail, we need to keep doing try and catch

--- wolframclient/language/decorators.py
+++ wolframclient/language/decorators.py
@@ -4,28 +4,32 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 from functools import wraps
 
-from wolframclient.language.exceptions import WolframLanguageExceptionBase, WolframLanguageExceptionFromPython
+from wolframclient.language.exceptions import WolframLanguageException
 from wolframclient.serializers import export
 
 import sys
 
-def safe_wl_execute(function, args = (), opts = {}, export_opts = {}, exception_class = WolframLanguageExceptionFromPython):
+def safe_wl_execute(function, args = (), opts = {}, export_opts = {}, exception_class = WolframLanguageException):
     try:
         return export(function(*args, **opts), **export_opts)
-    except WolframLanguageExceptionBase as e:
-        e.set_traceback(*sys.exc_info())
-        return export(e, **export_opts)
     except Exception as e:
 
         #the user might provide an exception class, that might be broken.
         #in this case we are running another try / except to return errors that are happneing during class serialization
 
-        if exception_class is WolframLanguageExceptionFromPython:
-            return export(WolframLanguageExceptionFromPython(e, exec_info = sys.exc_info()), **export_opts)
+        if isinstance(e, WolframLanguageException):
+            try:
+                e.set_traceback(*sys.exc_info())
+                return export(e, **export_opts)  
+            except Exception as e:
+                pass
+
+        if exception_class is WolframLanguageException:
+            return export(WolframLanguageException(e, exec_info = sys.exc_info()), **export_opts)
         try:
             return export(exception_class(e, exec_info = sys.exc_info()), **export_opts)
         except Exception as e:
-            return export(WolframLanguageExceptionFromPython(e, exec_info = sys.exc_info()), **export_opts)
+            return export(WolframLanguageException(e, exec_info = sys.exc_info()), **export_opts)
 
 def to_wl(**export_opts):
     def outer(function):
