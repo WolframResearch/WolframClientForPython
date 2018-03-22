adb8cca1fdb54b64d2dc786b6a5e87dd2c224df2
riccardodivirgilio <riccardod@wolfram.com>
2018-03-22 16:00:42 +0100

    adding cached_property

--- wolframclient/utils/decorators.py
+++ wolframclient/utils/decorators.py
@@ -15,3 +15,22 @@ def decorate(*func):
 
 to_tuple = decorate(tuple)
 to_dict  = decorate(Association)
+
+class cached_property(object):
+    """
+    Decorator that converts a method with a single self argument into a
+    property cached on the instance.
+
+    Optional ``name`` argument allows you to make cached properties of other
+    methods. (e.g.  url = cached_property(get_absolute_url, name='url') )
+    """
+    def __init__(self, func, name=None):
+        self.func = func
+        self.__doc__ = getattr(func, '__doc__')
+        self.name = name or func.__name__
+
+    def __get__(self, instance, cls=None):
+        if instance is None:
+            return self
+        res = instance.__dict__[self.name] = self.func(instance)
+        return res
