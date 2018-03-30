56e3964dcb74b2d87d4a7b6750fe20398adcc333
riccardodivirgilio <riccardod@wolfram.com>
2018-03-30 17:14:10 +0200

    adding py34

--- wolframclient/utils/six.py
+++ wolframclient/utils/six.py
@@ -11,8 +11,9 @@ import types
 
 #stripped version of SIX
 
-PY2 = sys.version_info[0] == 2
-PY3 = sys.version_info[0] == 3
+PY2  = sys.version_info[0] == 2
+PY3  = sys.version_info[0] == 3
+PY34 = sys.version_info[0:2] <= (3, 4)
 
 JYTHON = sys.platform.startswith('java')
 
