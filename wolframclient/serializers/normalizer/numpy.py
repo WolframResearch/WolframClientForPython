07a32dc61c6ea5446d159f1dc6acbb5360c0c10c
riccardodivirgilio <riccardod@wolfram.com>
2018-04-12 16:49:43 +0200

    using dtype

--- wolframclient/serializers/normalizer/numpy.py
+++ wolframclient/serializers/normalizer/numpy.py
@@ -9,18 +9,18 @@ from wolframclient.utils.functional import identity
 import numpy
 
 NUMPY_MAPPING = {
-    numpy.int8:       ('Integer8',          methodcaller('astype', '<i1')),
-    numpy.int16:      ('Integer16',         methodcaller('astype', '<i2')),
-    numpy.int32:      ('Integer32',         methodcaller('astype', '<i4')),
-    numpy.int64:      ('Integer64',         methodcaller('astype', '<i8')),
-    numpy.uint8:      ('UnsignedInteger8',  methodcaller('astype', '<u1')),
-    numpy.uint16:     ('UnsignedInteger16', methodcaller('astype', '<u2')),
-    numpy.uint32:     ('UnsignedInteger32', methodcaller('astype', '<u4')),
-    numpy.uint64:     ('UnsignedInteger64', methodcaller('astype', '<u8')),
-    numpy.float32:    ('Real32',            identity),
-    numpy.float64:    ('Real64',            identity),
-    numpy.complex64:  ('ComplexReal32',     identity),
-    numpy.complex128: ('ComplexReal64',     identity),
+    numpy.dtype('int8'):       ('Integer8',          methodcaller('astype', '<i1')),
+    numpy.dtype('int16'):      ('Integer16',         methodcaller('astype', '<i2')),
+    numpy.dtype('int32'):      ('Integer32',         methodcaller('astype', '<i4')),
+    numpy.dtype('int64'):      ('Integer64',         methodcaller('astype', '<i8')),
+    numpy.dtype('uint8'):      ('UnsignedInteger8',  methodcaller('astype', '<u1')),
+    numpy.dtype('uint16'):     ('UnsignedInteger16', methodcaller('astype', '<u2')),
+    numpy.dtype('uint32'):     ('UnsignedInteger32', methodcaller('astype', '<u4')),
+    numpy.dtype('uint64'):     ('UnsignedInteger64', methodcaller('astype', '<u8')),
+    numpy.dtype('float32'):    ('Real32',            identity),
+    numpy.dtype('float64'):    ('Real64',            identity),
+    numpy.dtype('complex64'):  ('ComplexReal32',     identity),
+    numpy.dtype('complex128'): ('ComplexReal64',     identity),
 }
 
 def update_dispatch(dispatch):
@@ -29,20 +29,16 @@ def update_dispatch(dispatch):
     def normalizer(self, o):
 
         try:
-            value_type, handler = NUMPY_MAPPING[o.dtype]
+            wl_type, handler = NUMPY_MAPPING[o.dtype]
         except KeyError:
-            raise NotImplementedError('NumPy serialization not implemented for %s' % repr(o.dtype))
+            raise NotImplementedError('NumPy serialization not implemented for %s. Choices are: %s' % (
+                repr(o.dtype),
+                ', '.join(map(repr, NUMPY_MAPPING.keys()))
+            ))
 
         data = handler(o)
 
-        if hasattr(data, 'tobytes'):
-            #Numpy 1.9+ support array.tobytes, but previous versions don't and use tostring instead.
-            data = data.tobytes()
-        else:
-            data = data.tostring()
-
-        return self.serialize_raw_array(
+        return self.serialize_ndarray(
             data,
-            o.shape,
-            value_type,
+            wl_type
         )
