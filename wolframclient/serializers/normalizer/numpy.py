b9e8b2ecd46527cced235971b623d50f84478515
riccardodivirgilio <riccardod@wolfram.com>
2018-04-12 15:31:34 +0200

    removing ARRAY_TYPE

--- wolframclient/serializers/normalizer/numpy.py
+++ wolframclient/serializers/normalizer/numpy.py
@@ -4,24 +4,23 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 from operator import methodcaller
 
-from wolframclient.serializers.wxfencoder.wxfexpr import ARRAY_TYPES
 from wolframclient.utils.functional import identity
 
 import numpy
 
 NUMPY_MAPPING = {
-    numpy.int8:       (ARRAY_TYPES.Integer8,          methodcaller('astype', '<i1')),
-    numpy.int16:      (ARRAY_TYPES.Integer16,         methodcaller('astype', '<i2')),
-    numpy.int32:      (ARRAY_TYPES.Integer32,         methodcaller('astype', '<i4')),
-    numpy.int64:      (ARRAY_TYPES.Integer64,         methodcaller('astype', '<i8')),
-    numpy.uint8:      (ARRAY_TYPES.UnsignedInteger8,  methodcaller('astype', '<u1')),
-    numpy.uint16:     (ARRAY_TYPES.UnsignedInteger16, methodcaller('astype', '<u2')),
-    numpy.uint32:     (ARRAY_TYPES.UnsignedInteger32, methodcaller('astype', '<u4')),
-    numpy.uint64:     (ARRAY_TYPES.UnsignedInteger64, methodcaller('astype', '<u8')),
-    numpy.float32:    (ARRAY_TYPES.Real32,            identity),
-    numpy.float64:    (ARRAY_TYPES.Real64,            identity),
-    numpy.complex64:  (ARRAY_TYPES.ComplexReal32,     identity),
-    numpy.complex128: (ARRAY_TYPES.ComplexReal64,     identity),
+    numpy.int8:       ('Integer8',          methodcaller('astype', '<i1')),
+    numpy.int16:      ('Integer16',         methodcaller('astype', '<i2')),
+    numpy.int32:      ('Integer32',         methodcaller('astype', '<i4')),
+    numpy.int64:      ('Integer64',         methodcaller('astype', '<i8')),
+    numpy.uint8:      ('UnsignedInteger8',  methodcaller('astype', '<u1')),
+    numpy.uint16:     ('UnsignedInteger16', methodcaller('astype', '<u2')),
+    numpy.uint32:     ('UnsignedInteger32', methodcaller('astype', '<u4')),
+    numpy.uint64:     ('UnsignedInteger64', methodcaller('astype', '<u8')),
+    numpy.float32:    ('Real32',            identity),
+    numpy.float64:    ('Real64',            identity),
+    numpy.complex64:  ('ComplexReal32',     identity),
+    numpy.complex128: ('ComplexReal64',     identity),
 }
 
 def update_dispatch(dispatch):
@@ -43,7 +42,7 @@ def update_dispatch(dispatch):
             yield array_class(python_expr.shape, value_type, data.tostring())
 
         return self.serialize_raw_array(
+            data,
             o.shape,
             value_type,
-            data
         )
