fc3055f9002994befeb3bf08896cfd09aeb6522f
Dorian Birraux <dorianb@wolfram.com>
2018-06-01 15:14:50 +0200

    Define local kernel exposed api.

--- wolframclient/evaluation/kernel/__init__.py
+++ wolframclient/evaluation/kernel/__init__.py
@@ -1,1 +1,7 @@
 # -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from wolframclient.evaluation.kernel import kernelsession
+
+__all__ = kernelsession.__all__
+
+from wolframclient.evaluation.kernel.kernelsession import *
