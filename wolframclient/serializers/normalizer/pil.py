203963e40bf8e5dc29013d83116707906d770d6a
riccardodivirgilio <riccardodivirgilio@gmail.com>
2018-06-15 15:26:49 +0200

    auto sorting

--- wolframclient/serializers/normalizer/pil.py
+++ wolframclient/serializers/normalizer/pil.py
@@ -2,9 +2,8 @@
 
 from __future__ import absolute_import, print_function, unicode_literals
 
-from wolframclient.utils.api import PIL
-
 from wolframclient.utils import six
+from wolframclient.utils.api import PIL
 
 def update_dispatch(dispatch):
 
