e16726db91a68103d9fd37924032e633d59cb9fa
riccardodivirgilio <riccardodivirgilio@gmail.com>
2018-06-15 15:16:52 +0200

    importing from the real location

--- wolframclient/language/decorators.py
+++ wolframclient/language/decorators.py
@@ -6,8 +6,7 @@ from functools import wraps
 
 from wolframclient.language.exceptions import WolframLanguageException
 from wolframclient.language.expression import wl
-from wolframclient.serializers import export
-from wolframclient.serializers.export import DEFAULT_FORMAT
+from wolframclient.serializers.export import DEFAULT_FORMAT, export
 from wolframclient.utils.encoding import force_text, safe_force_text
 
 import sys
