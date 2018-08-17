e50a3d2d8f24877758de88f406b54e59054f2e00
Dorian Birraux <dorianb@wolfram.com>
2018-08-17 17:32:01 +0200

    changing a bit how default logger is specified. Add UTF8 encoding to avoid Windows Unicode error. Use Python2 compatible named parameters.

--- wolframclient/logger/utils.py
+++ wolframclient/logger/utils.py
@@ -1,13 +1,21 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import absolute_import, print_function, unicode_literals
-
+from wolframclient.utils.six import PY2
 import logging
 
-def setup_logging_to_file(path, level=logging.INFO):
-    """Setup a basic Python logging configuration to a given file."""
-    logging.basicConfig(filename=path,
-        filemode='a',
-        format='%(asctime)s, %(name)s %(levelname)s %(message)s',
-        level=level
-    )
+if PY2:
+    def setup_logging_to_file(path, level=logging.INFO):
+        logging.basicConfig(filename=path,
+            filemode='a',
+            format='%(asctime)s, %(name)s %(levelname)s %(message)s',
+            level=level
+        )
+else:
+    def setup_logging_to_file(path, level=logging.INFO):
+        """Setup a basic Python logging configuration to a given file."""
+        logging.basicConfig(
+            format=u'%(asctime)s, %(name)s %(levelname)s %(message)s',
+            handlers=[logging.FileHandler(path, 'a', 'utf-8')],
+            level=level
+        )
