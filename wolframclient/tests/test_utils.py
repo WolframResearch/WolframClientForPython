5031bf639b77bce77e2d99d31ca91f64dbcc7044
Dorian Birraux <dorianb@wolfram.com>
2018-09-05 16:52:19 +0200

    Some test for utility functions.

---
+++ wolframclient/tests/test_utils.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, print_function, unicode_literals
+
+from wolframclient.utils.tests import TestCase as BaseTestCase
+from wolframclient.logger.utils import str_trim
+
+class TestCase(BaseTestCase):
+
+    def test_str_trim_no_limit(self):
+        self.assertEqual(str_trim('abcde'), 'abcde')
+
+    def test_str_trim_above_limit(self):
+        self.assertEqual(str_trim('abcde', max_char=3), 'abc...(2 more)')
+
+    def test_str_trim_eq_limit(self):
+        self.assertEqual(str_trim('abc', max_char=3), 'abc')
