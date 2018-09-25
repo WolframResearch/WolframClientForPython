6ebfae150e8dfab5e178c752605163e488ac0800
Dorian Birraux <dorianb@wolfram.com>
2018-09-25 17:02:21 +0200

    Fix JSON in utils. Resolve name ambiguity.

--- wolframclient/utils/json.py
+++ wolframclient/utils/json.py
@@ -2,17 +2,15 @@
 
 from __future__ import absolute_import, print_function, unicode_literals
 
-from wolframclient.utils.encoding import force_text
-from wolframclient.utils.six import binary_type
-
-import json
+from json import loads as json_loads
 import sys
-
+from wolframclient.utils.encoding import force_text
+from wolframclient.utils.six import string_types, binary_type
 # Python 3.4 and 3.5 json loads only accept str.
 if sys.version_info[0] == 3 and sys.version_info[1] <= 5:
     def loads(s, **kwargs):
         if isinstance(s, binary_type):
             s = force_text(s)
-        return json.loads(s)
+        return json_loads(s)
 else:
-    loads = json.loads
+    loads = json_loads
