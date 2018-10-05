c0ac235002b788697121b1942a8c404364937aa4
Riccardo Di Virgilio <riccardod@wolfram.com>
2018-10-05 13:19:30 +0200

    adding auto refactor script

---
+++ wolframclient/cli/commands/refactor.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, print_function, unicode_literals
+
+from wolframclient.cli.utils import SimpleCommand
+from wolframclient.utils.require import require
+from wolframclient.utils.importutils import module_path
+
+import sys
+
+class Command(SimpleCommand):
+
+    modules = ['wolframclient']
+
+    def _module_args(self, *args):
+
+        yield __file__ #autopep main is dropping the first argument
+
+        for module in self.modules:
+            yield module_path(module)
+
+        for arg in args:
+            yield arg
+
+    @require('autopep8', 'isort', 'autoflake')
+    def handle(self, *args):
+
+        #https://pypi.org/project/isort/
+
+        from autoflake import main
+
+        sys.argv = tuple(self._module_args('--in-place', '--remove-duplicate-keys', '--expand-star-import', '--remove-all-unused-imports', '--recursive'))
+
+        main()
+
+        from isort.main import main
+
+        sys.argv = list(self._module_args('-rc', '--multi-line', '5', '-a', "from __future__ import absolute_import, print_function, unicode_literals"))
+
+        main()
+
+        from autopep8 import main
+
+        sys.argv = tuple(self._module_args('--in-place', '--aggressive', '--recursive'))
+
+        main()
+
