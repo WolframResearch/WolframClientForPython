9d2d6db2c409febb6388fed781f55764079e1566
Riccardo Di Virgilio <riccardod@wolfram.com>
2018-10-05 13:19:30 +0200

    adding auto refactor script

--- wolframclient/cli/commands/refactor.py
+++ wolframclient/cli/commands/refactor.py
@@ -2,22 +2,19 @@
 
 from __future__ import absolute_import, print_function, unicode_literals
 
-import sys
-
 from wolframclient.cli.utils import SimpleCommand
+from wolframclient.utils.require import require
 from wolframclient.utils.importutils import module_path
 
+import sys
 
 class Command(SimpleCommand):
 
     modules = ['wolframclient']
 
-    dependencies = (('autopep8', '1.4'), ('isort', '4.3.4'),
-                    ('yapf', '0.24.0'), ('autoflake', '1.2'))
-
     def _module_args(self, *args):
 
-        yield __file__  # autopep main is dropping the first argument
+        yield __file__ #autopep main is dropping the first argument
 
         for module in self.modules:
             yield module_path(module)
@@ -25,34 +22,26 @@ class Command(SimpleCommand):
         for arg in args:
             yield arg
 
-    def handle(self, **opts):
+    @require('autopep8', 'isort', 'autoflake')
+    def handle(self, *args):
 
-        argv = sys.argv
+        #https://pypi.org/project/isort/
 
         from autoflake import main
 
-        sys.argv = tuple(
-            self._module_args('--in-place', '--remove-duplicate-keys',
-                              '--expand-star-import',
-                              '--remove-all-unused-imports', '--recursive'))
+        sys.argv = tuple(self._module_args('--in-place', '--remove-duplicate-keys', '--expand-star-import', '--remove-all-unused-imports', '--recursive'))
 
         main()
 
         from isort.main import main
 
-        sys.argv = list(
-            self._module_args(
-                '-rc', '--multi-line', '5', '-a',
-                "from __future__ import absolute_import, print_function, unicode_literals"
-            ))
+        sys.argv = list(self._module_args('-rc', '--multi-line', '5', '-a', "from __future__ import absolute_import, print_function, unicode_literals"))
 
         main()
 
-        import yapf
+        from autopep8 import main
 
-        sys.argv = list(
-            self._module_args('--in-place', '--recursive', '--parallel'))
+        sys.argv = tuple(self._module_args('--in-place', '--aggressive', '--recursive'))
 
-        yapf.run_main()
+        main()
 
-        sys.argv = argv
