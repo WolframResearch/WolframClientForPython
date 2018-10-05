deb98d020ee106917842ccb0cedcbe748c9e2490
Dorian Birraux <dorianb@wolfram.com>
2018-10-05 15:41:47 +0200

    Fix doc building process after coroutine update..

--- docs/roles/__init__.py
+++ docs/roles/__init__.py
@@ -1,5 +1,6 @@
-
 # -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, print_function, unicode_literals
 from roles.wl import wl_reference_role
 
 def setup(app):
--- docs/roles/wl.py
+++ docs/roles/wl.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, print_function, unicode_literals
 from docutils import nodes, utils
 from docutils.parsers.rst import roles
 
