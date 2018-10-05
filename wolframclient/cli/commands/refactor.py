f7fe401ad45d3fb5ee03a79d1f413f5b6bc9309e
Riccardo Di Virgilio <riccardod@wolfram.com>
2018-10-05 13:22:07 +0200

    adding version number

--- wolframclient/cli/commands/refactor.py
+++ wolframclient/cli/commands/refactor.py
@@ -2,11 +2,12 @@
 
 from __future__ import absolute_import, print_function, unicode_literals
 
-from wolframclient.cli.utils import SimpleCommand
-from wolframclient.utils.require import require
-from wolframclient.utils.importutils import module_path
-
 import sys
+
+from wolframclient.cli.utils import SimpleCommand
+from wolframclient.utils.importutils import module_path
+from wolframclient.utils.require import require
+
 
 class Command(SimpleCommand):
 
@@ -14,7 +15,7 @@ class Command(SimpleCommand):
 
     def _module_args(self, *args):
 
-        yield __file__ #autopep main is dropping the first argument
+        yield __file__  # autopep main is dropping the first argument
 
         for module in self.modules:
             yield module_path(module)
@@ -22,26 +23,41 @@ class Command(SimpleCommand):
         for arg in args:
             yield arg
 
-    @require('autopep8', 'isort', 'autoflake')
+    @require(('autopep8', '1.4'), ('isort', '4.3.4'), ('autoflake', '1.2'))
     def handle(self, *args):
 
-        #https://pypi.org/project/isort/
+        # https://pypi.org/project/isort/
 
         from autoflake import main
 
-        sys.argv = tuple(self._module_args('--in-place', '--remove-duplicate-keys', '--expand-star-import', '--remove-all-unused-imports', '--recursive'))
+        sys.argv = tuple(
+            self._module_args(
+                '--in-place',
+                '--remove-duplicate-keys',
+                '--expand-star-import',
+                '--remove-all-unused-imports',
+                '--recursive'))
 
         main()
 
         from isort.main import main
 
-        sys.argv = list(self._module_args('-rc', '--multi-line', '5', '-a', "from __future__ import absolute_import, print_function, unicode_literals"))
+        sys.argv = list(
+            self._module_args(
+                '-rc',
+                '--multi-line',
+                '5',
+                '-a',
+                "from __future__ import absolute_import, print_function, unicode_literals"))
 
         main()
 
         from autopep8 import main
 
-        sys.argv = tuple(self._module_args('--in-place', '--aggressive', '--recursive'))
+        sys.argv = tuple(
+            self._module_args(
+                '--in-place',
+                '--aggressive',
+                '--recursive'))
 
         main()
-
