92cdc6700f6b190b57938bd0cedf278c191acb26
Riccardo Di Virgilio <riccardod@wolfram.com>
2018-10-16 16:45:09 -0500

    removing duplicated code

--- wolframclient/tests/serializers/wxf_numpy.py
+++ wolframclient/tests/serializers/wxf_numpy.py
@@ -32,6 +32,10 @@ class TestCase(BaseTestCase):
     def initOnlyRA(cls):
         return cls.init(False, True)
 
+    def compare_serializer(self, serializer, array, value):
+        serializer.serialize(array)
+        self.assertEqual(serializer._writer.getvalue(), value)
+
     @staticmethod
     def init(pa, ra):
         expr_provider = WXFExprProvider()
@@ -40,9 +44,9 @@ class TestCase(BaseTestCase):
 
         expr_provider.add_encoder(numpy_encoder)
         expr_provider.add_encoder(DefaultWXFEncoder())
-        stream = six.BytesIO()
-        serializer = WXFExprSerializer(stream, expr_provider=expr_provider)
-        return (serializer, stream)
+        serializer = WXFExprSerializer(
+            six.BytesIO(), expr_provider=expr_provider)
+        return serializer
 
     def test_dimensions(self):
         provider = WXFExprProvider(NumPyWXFEncoder())
@@ -60,98 +64,89 @@ class TestCase(BaseTestCase):
             str(err.exception), "Dimensions must be positive integers.")
 
     def test_int8_PA(self):
-        s, d = self.initDefault()
-        arr = numpy.array([[-(1 << 7), -1], [1, (1 << 7) - 1]], numpy.int8)
-        s.serialize(arr)
-        self.assertEqual(d.getvalue(),
-                         b'\x38\x3a\xc1\x00\x02\x02\x02\x80\xff\x01\x7f')
+
+        self.compare_serializer(
+            self.initDefault(),
+            numpy.array([[-(1 << 7), -1], [1, (1 << 7) - 1]], numpy.int8),
+            b'\x38\x3a\xc1\x00\x02\x02\x02\x80\xff\x01\x7f')
 
     def test_int8_Both(self):
-        s, d = self.initBothArraySupport()
-        arr = numpy.array([[-(1 << 7), -1], [1, (1 << 7) - 1]], numpy.int8)
-        s.serialize(arr)
-        self.assertEqual(d.getvalue(),
-                         b'\x38\x3a\xc1\x00\x02\x02\x02\x80\xff\x01\x7f')
+
+        self.compare_serializer(
+            self.initBothArraySupport(),
+            numpy.array([[-(1 << 7), -1], [1, (1 << 7) - 1]], numpy.int8),
+            b'\x38\x3a\xc1\x00\x02\x02\x02\x80\xff\x01\x7f')
 
     def test_int8_RA(self):
-        s, d = self.initOnlyRA()
-        arr = numpy.array([[-(1 << 7), -1], [1, (1 << 7) - 1]], numpy.int8)
-        s.serialize(arr)
-        self.assertEqual(d.getvalue(),
-                         b'\x38\x3a\xc2\x00\x02\x02\x02\x80\xff\x01\x7f')
+
+        self.compare_serializer(
+            self.initOnlyRA(),
+            numpy.array([[-(1 << 7), -1], [1, (1 << 7) - 1]], numpy.int8),
+            b'\x38\x3a\xc2\x00\x02\x02\x02\x80\xff\x01\x7f')
 
     def test_int16(self):
-        s, d = self.initDefault()
-        sRA, dRA = self.initOnlyRA()
+        s = self.initDefault()
+        sRA = self.initOnlyRA()
         arr = arr = numpy.array([[-(1 << 15)], [(1 << 15) - 1]], numpy.int16)
-        s.serialize(arr)
-        sRA.serialize(arr)
-        self.assertEqual(d.getvalue(),
-                         b'8:\xc1\x01\x02\x02\x01\x00\x80\xff\x7f')
-        self.assertEqual(dRA.getvalue(),
-                         b'8:\xc2\x01\x02\x02\x01\x00\x80\xff\x7f')
+
+        self.compare_serializer(s, arr,
+                                b'8:\xc1\x01\x02\x02\x01\x00\x80\xff\x7f')
+        self.compare_serializer(sRA, arr,
+                                b'8:\xc2\x01\x02\x02\x01\x00\x80\xff\x7f')
 
     def test_int32(self):
-        s, d = self.initDefault()
-        sRA, dRA = self.initOnlyRA()
-        arr = arr = numpy.array([[-(1 << 31)], [(1 << 31) - 1]], numpy.int32)
-        s.serialize(arr)
-        sRA.serialize(arr)
-        self.assertEqual(
-            d.getvalue(),
+
+        arr = numpy.array([[-(1 << 31)], [(1 << 31) - 1]], numpy.int32)
+
+        self.compare_serializer(
+            self.initDefault(), arr,
             b'8:\xc1\x02\x02\x02\x01\x00\x00\x00\x80\xff\xff\xff\x7f')
-        self.assertEqual(
-            dRA.getvalue(),
+        self.compare_serializer(
+            self.initOnlyRA(), arr,
             b'8:\xc2\x02\x02\x02\x01\x00\x00\x00\x80\xff\xff\xff\x7f')
 
     def test_int64(self):
-        s, d = self.initDefault()
-        sRA, dRA = self.initOnlyRA()
-        arr = arr = numpy.array([[-(1 << 62)], [(1 << 62)]], numpy.int64)
-        s.serialize(arr)
-        sRA.serialize(arr)
-        self.assertEqual(
-            d.getvalue(),
+
+        arr = numpy.array([[-(1 << 62)], [(1 << 62)]], numpy.int64)
+        self.compare_serializer(
+            self.initDefault(), arr,
             b'8:\xc1\x03\x02\x02\x01\x00\x00\x00\x00\x00\x00\x00\xc0\x00\x00\x00\x00\x00\x00\x00@'
         )
-        self.assertEqual(
-            dRA.getvalue(),
+        self.compare_serializer(
+            self.initOnlyRA(), arr,
             b'8:\xc2\x03\x02\x02\x01\x00\x00\x00\x00\x00\x00\x00\xc0\x00\x00\x00\x00\x00\x00\x00@'
         )
 
     def test_uint8_PA(self):
-        s, d = self.initDefault()
-        arr = numpy.array([[0, (1 << 7)]], numpy.uint8)
-        s.serialize(arr)
-        self.assertEqual(d.getvalue(),
-                         b'\x38\x3a\xc1\x01\x02\x01\x02\x00\x00\x80\x00')
+
+        self.compare_serializer(
+            self.initDefault(), numpy.array([[0, (1 << 7)]], numpy.uint8),
+            b'\x38\x3a\xc1\x01\x02\x01\x02\x00\x00\x80\x00')
 
     def test_uint8_RA(self):
-        s, d = self.initBothArraySupport()
-        arr = numpy.array([0, (1 << 8) - 1], numpy.uint8)
-        s.serialize(arr)
-        self.assertEqual(d.getvalue(), b'8:\xc2\x10\x01\x02\x00\xff')
+
+        self.compare_serializer(self.initBothArraySupport(),
+                                numpy.array([0, (1 << 8) - 1], numpy.uint8),
+                                b'8:\xc2\x10\x01\x02\x00\xff')
 
     def test_uint16_RA(self):
-        s, d = self.initBothArraySupport()
-        arr = numpy.array([0, (1 << 16) - 1], numpy.uint16)
-        s.serialize(arr)
-        self.assertEqual(d.getvalue(), b'8:\xc2\x11\x01\x02\x00\x00\xff\xff')
+
+        self.compare_serializer(self.initBothArraySupport(),
+                                numpy.array([0, (1 << 16) - 1], numpy.uint16),
+                                b'8:\xc2\x11\x01\x02\x00\x00\xff\xff')
 
     def test_uint32_RA(self):
-        s, d = self.initBothArraySupport()
-        arr = numpy.array([0, (1 << 32) - 1], numpy.uint32)
-        s.serialize(arr)
-        self.assertEqual(
-            d.getvalue(),
+
+        self.compare_serializer(
+            self.initBothArraySupport(),
+            numpy.array([0, (1 << 32) - 1], numpy.uint32),
             b'8:\xc2\x12\x01\x02\x00\x00\x00\x00\xff\xff\xff\xff')
 
     def test_uint64_RA(self):
-        s, d = self.initBothArraySupport()
-        arr = numpy.array([0, (1 << 64) - 1], numpy.uint64)
-        s.serialize(arr)
-        self.assertEqual(
-            d.getvalue(),
+
+        self.compare_serializer(
+            self.initBothArraySupport(),
+            numpy.array([0, (1 << 64) - 1], numpy.uint64),
             b'8:\xc2\x13\x01\x02\x00\x00\x00\x00\x00\x00\x00\x00\xff\xff\xff\xff\xff\xff\xff\xff'
         )
 
