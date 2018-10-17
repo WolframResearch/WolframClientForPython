1590eaeff65d6dfb2fff5c6a53a7b1ee5bb3e753
Riccardo Di Virgilio <riccardod@wolfram.com>
2018-10-17 13:14:06 -0500

    this is not needed anymore

--- wolframclient/utils/tests.py
+++ wolframclient/utils/tests.py
@@ -7,8 +7,4 @@ import unittest
 
 
 class TestCase(unittest.TestCase):
-    @classmethod
-    def discover_tests(cls, pattern=re.compile('^test_[A-Za-z_0-9-]+$')):
-        for attribute in dir(cls):
-            if pattern.match(attribute):
-                yield attribute
+    pass
