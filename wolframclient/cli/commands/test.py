f482ba934f95be0ea10487b2dc00eb5021b78908
Riccardo Di Virgilio <riccardod@wolfram.com>
2018-10-17 13:07:15 -0500

    keep using modules instead of just __file__

--- wolframclient/cli/commands/test.py
+++ wolframclient/cli/commands/test.py
@@ -7,35 +7,28 @@ import sys
 import unittest
 
 from wolframclient.cli.utils import SimpleCommand
-
+from wolframclient.utils.importutils import module_path
 
 class Command(SimpleCommand):
     """ Run test suites from the tests modules.
     A list of patterns can be provided to specify the tests to run.
     """
 
+    modules = ['wolframclient.tests']
+
     def add_arguments(self, parser):
         parser.add_argument('args', nargs='*')
 
     def handle(self, *args):
-        here = os.path.abspath(os.path.dirname(__file__))
-        project_root = os.path.dirname(os.path.dirname(here))
 
-        # args can be a list of patterns.
-        if args:
-            suite = unittest.TestSuite()
-            for arg in args:
+        suite = unittest.TestSuite()
+        for root in map(module_path, self.modules):
+            for arg in args or ['*']:
                 suite.addTests(
                     unittest.defaultTestLoader.discover(
-                        os.path.join(project_root, 'tests'),
+                        root,
                         pattern=arg,
-                        top_level_dir=project_root))
-        # take every single test from tests module.
-        else:
-            suite = unittest.defaultTestLoader.discover(
-                os.path.join(project_root, 'tests'),
-                pattern='*',
-                top_level_dir=project_root)
+                        top_level_dir=root))
 
         # verbosity > 1 print test name
         runner = unittest.TextTestRunner(verbosity=2)
