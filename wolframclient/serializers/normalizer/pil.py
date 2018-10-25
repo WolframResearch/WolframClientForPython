ffd7a5c30a30a0dcc3286e1092470c6dcd80b84f
Garrett Ducharme <garrett@wolfram.com>
2018-10-25 15:45:56 -0500

    Import PIL would hang, this fixes it
    Issue with loading pil and calling to_wl at the same time.
    This errors because it tries to import something that doesn't
    exist. I tried to add support using the API() method some of the
    other libraries use, but couldn't get it to work. So there is likely
    a more robust way, but just having the import at the top seems to only
    load when needed, and work in all the cases I tried.

--- wolframclient/serializers/normalizer/pil.py
+++ wolframclient/serializers/normalizer/pil.py
@@ -3,14 +3,14 @@
 from __future__ import absolute_import, print_function, unicode_literals
 
 from wolframclient.utils import six
-from wolframclient.utils.api import PIL
+import PIL.Image
 
 
 def update_dispatch(dispatch):
     @dispatch.multi(PIL.Image.Image)
     def normalizer(self, im):
         stream = six.BytesIO()
-        im.save(stream, target_format='png')
+        im.save(stream, "PNG")
 
         return self.serialize_function(
             self.serialize_symbol(b'ImportByteArray'),
