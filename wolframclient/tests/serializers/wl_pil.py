d7ba8ded94fb27bf816c2725dd38f10a45a203e6
Riccardo Di Virgilio <riccardod@wolfram.com>
2018-10-26 15:00:54 +0200

    adding pil test suite

---
+++ wolframclient/tests/serializers/wl_pil.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, print_function, unicode_literals
+
+from wolframclient.serializers import export
+from wolframclient.utils.api import PIL
+from wolframclient.utils.importutils import module_path
+from wolframclient.utils.tests import TestCase as BaseTestCase
+
+
+class TestCase(BaseTestCase):
+    def test_pil(self):
+
+        image = PIL.open(
+            module_path('wolframclient', 'tests', 'data', '5x2.png'))
+
+        self.assertEqual(
+            export(image, target_format='wl'),
+            b'ImportByteArray[ByteArray["iVBORw0KGgoAAAANSUhEUgAAAAUAAAACEAAAAADlkZXCAAAAH0lEQVR4nGP0+P39rf6+ky9/R7Aoen2+9shDWSRCHwCO7ws73c3PRQAAAABJRU5ErkJggg=="], "PNG"]'
+        )
