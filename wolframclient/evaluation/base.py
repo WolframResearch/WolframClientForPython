f7524cd4e982dbeda60735ad1e7f1d36c64c1cf8
Riccardo Di Virgilio <riccardod@wolfram.com>
2018-10-29 16:54:50 +0100

    adding main class for all evaluators

---
+++ wolframclient/evaluation/base.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, print_function, unicode_literals
+
+from wolframclient.utils.async import wait_all, syncronous_wait_all
+from wolframclient.language.expression import WLFunction
+from wolframclient.utils.functional import first
+
+#SYNC VERSION
+
+class WolframEvaluator(WolframEvaluatorMixin):
+
+    def started(self):
+        raise NotImplementedError
+
+    def evaluate(self, expr):
+        return self.evaluate_wrap(expr).get()
+
+    def evaluate_many(self, expr_list):
+        return map(self.evaluate, expr_list)
+
+    def evaluate_now(self, expr):
+        raise NotImplementedError
+
+    def start(self):
+        raise NotImplementedError
+
+    def stop(self):
+        #Graceful stop
+        raise NotImplementedError
+
+    def terminate(self):
+        raise NotImplementedError
+
+    def restart(self):
+        if self.started():
+            self.stop()
+        self.start()
+
+    def function(self, expr):
+        def inner(self, *args, **opts):
+            return self.evaluate(WLFunction(expr, *args, **opts))
+        return inner
+
+    def __enter__(self):
+        if not self.started():
+            self.start()
+        return self
+
+    def __exit__(self, type, value, traceback):
+        if self.started():
+            self.stop()
+
+
+
+#ASYNC VERSION
+
+class WolframAsyncEvaluator(WolframEvaluatorMixin):
+
+    def __init__(self, loop = None):
+        self.loop = loop
+
+    async def started(self):
+        raise NotImplementedError
+
+    async def evaluate(self, expr):
+        return await self.evaluate_wrap(expr).get()
+
+    async def evaluate_many(self, expr_list):
+        return await wait_all(map(self.evaluate, expr_list), loop = self.loop)
+
+    async def evaluate_wrap(self, expr):
+        raise NotImplementedError
+
+    async def start(self):
+        raise NotImplementedError
+
+    async def stop(self):
+        #Graceful stop
+        raise NotImplementedError
+
+    async def terminate(self):
+        raise NotImplementedError
+
+    async def restart(self):
+        if await self.started():
+            await self.stop()
+        await self.start()
+
+    def function(self, expr):
+        async def inner(self, *args, **opts):
+            return await self.evaluate(WLFunction(expr, *args, **opts))
+        return inner
+
+    def __aenter__(self):
+        if not await self.started():
+            await self.start()
+        return self
+
+    def __aexit__(self, type, value, traceback):
+        if await self.started():
+            await self.stop()
+
+"""
+class AsyncToSyncWolframEvalutor:
+
+    def started(self):
+        return first(syncronous_wait_all(super().started()))
+
+
+class WolframAsyncCloudKernel(WolframAsyncEvaluator):
+    pass
+
+class WolframCloudKernel(AsyncToSyncWolframEvalutor, WolframAsyncEvaluator):
+    pass
+"""
+
+
+
+
+
