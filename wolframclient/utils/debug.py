5a176630305a0a95f133df6a724bf6a375efc4e7
Dorian Birraux <dorianb@wolfram.com>
2018-12-19 15:26:25 +0100

    Add repeated timing functions.
    Similarly to WL RepeatedTiming, repeated_timing execute a function N times
    and return a trimmed mean of the execution time.
    Add annotation @timed_repeated doing exactly the same.

--- wolframclient/utils/debug.py
+++ wolframclient/utils/debug.py
@@ -21,6 +21,26 @@ def echo(x):
     print(x)
     return x
 
+def timed_repeated(N=30):
+    def repeated(function):
+        def inner(*args, **opts):
+            return repeated_timing(function, *args, N=N, **opts)
+
+        return inner
+
+    return repeated
+
+def repeated_timing(function, *args, N=20, **opts):
+    timed_func = timed(function)
+    timers = []
+    for _ in range(N):
+        timer, res = timed_func(*args, **opts)
+        timers.append(timer)
+    timers = sorted(timers)
+    min = int(.1 * N)
+    max = int(.9 * N)
+    trimmed_timers = timers[min:max]
+    return sum(trimmed_timers)/len(trimmed_timers), res
 
 def print_elapsed_time(viewfunc):
     @wraps(viewfunc)
