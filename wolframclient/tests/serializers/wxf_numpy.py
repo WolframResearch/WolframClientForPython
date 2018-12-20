dd6d29be5122169c114390c63998d46501c779f8
Riccardo Di Virgilio <riccardod@wolfram.com>
2018-12-20 21:27:56 +0100

    fixing tests

--- wolframclient/tests/serializers/wxf_numpy.py
+++ wolframclient/tests/serializers/wxf_numpy.py
@@ -164,20 +164,20 @@ class TestCase(BaseTestCase):
 
     def test_numpy_float16(self):
         f16 = numpy.float16('1.234e-3')
-        self.assertEqual(export(f16), b'0.001234')
+        self.assertEqual(export(f16), six.PY2 and b'0.0012341' or b'0.001234')
 
     def test_numpy_float32(self):
         f32 = numpy.float32('1.2345678e-1')
-        self.assertEqual(export(f32), b'0.12345678')
+        self.assertEqual(export(f32), six.PY2 and b'0.123457' or b'0.12345678')
 
     def test_numpy_float64(self):
         f64 = numpy.float64('-1.234567891234e-1')
-        self.assertEqual(export(f64), b'-0.1234567891234')
+        self.assertEqual(export(f64), six.PY2 and b'-0.123456789123' or b'-0.1234567891234')
 
     def test_numpy_float128(self):
         f128 = numpy.float128('1.23e-1234')
         self.assertEqual(
-            export(f128), b'Times[0.5138393296552710333, Power[2, -4098]]')
+            export(f128), six.PY2 and b'Times[0.513839329655, Power[2, -4098]]' or b'Times[0.5138393296552710333, Power[2, -4098]]')
 
     def test_numpy_integers(self):
         int8 = [numpy.int8(127), numpy.int8(-128)]
