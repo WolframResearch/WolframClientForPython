40555c72c8e2f6c7b6d991fb7391dd45203949c8
Riccardo Di Virgilio <riccardod@wolfram.com>
2019-01-11 17:18:10 +0100

    adding simple test server

--- wolframclient/cli/commands/testserver.py
+++ wolframclient/cli/commands/testserver.py
@@ -14,26 +14,65 @@ async def generate_http_response(session, request, expression):
         wl.GenerateHTTPResponse(expression, request)(
             ("BodyByteArray", "Headers", "StatusCode")))
 
-def WolframEngine(path, poolsize = 1, **opts):
-    if poolsize <= 1:
-        return WolframLanguageAsyncSession(path, **opts)
-    return WolframEvaluatorPool(path, poolsize = poolsize, **opts)
-
 class Command(SimpleCommand):
     """ Run test suites from the tests modules.
     A list of patterns can be provided to specify the tests to run.
     """
 
-    port = 18000
-    poolsize = 4
 
-    def get_web_app(self, session):
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--get',
+            default=None,
+            help='Insert the string to Get.'
+        )        
+        parser.add_argument(
+            '--port',
+            default=18000,
+            help='Insert the port.'
+        )
+        parser.add_argument(
+            '--kernel',
+            default='/Applications/Mathematica.app/Contents/MacOS/WolframKernel',
+            help='Insert the kernel path.'
+        )
+        parser.add_argument(
+            '--poolsize',
+            default=4,
+            help='Insert the kernel pool size.'
+        )   
+        parser.add_argument(
+            '--autoreload',
+            default=False,
+            help='Insert the server should autoreload the WL input expression.',
+            action = 'store_true'
+        )
+
+    def create_session(self, path, poolsize = 1, **opts):
+        if poolsize <= 1:
+            return WolframLanguageAsyncSession(path, **opts)
+        return WolframEvaluatorPool(path, poolsize = poolsize, **opts)
+
+    def create_handler(self, get, autoreload):
+        if not get:
+            return wl.HTTPResponse("It works!")
+        if autoreload:
+            return wl.Get(get)
+        return wl.Once(wl.Get(get))
+
+    def get_web_app(self, kernel, poolsize, **opts):
+
+        session = self.create_session(
+            kernel, 
+            poolsize = poolsize
+        )
+        handler = self.create_handler(**opts)
 
         routes = web.RouteTableDef()
 
         @routes.get('/')
         async def hello(request):
-            response = await generate_http_response(session, request, wl.HTTPResponse("It works!"))
+            response = await generate_http_response(session, request, handler)
             return web.Response(
                 body=response['BodyByteArray'],
                 status=response['StatusCode'],
@@ -47,8 +86,8 @@ class Command(SimpleCommand):
     def exception_handler(self, exception, context):
         pass
 
-    def handle(self):
-
-        session = WolframEngine(
-            '/Applications/Mathematica.app/Contents/MacOS/WolframKernel', poolsize = self.poolsize)
-        web.run_app(self.get_web_app(session), port=self.port)
+    def handle(self, port, **opts):
+        web.run_app(
+            self.get_web_app(**opts), 
+            port=port
+        )
