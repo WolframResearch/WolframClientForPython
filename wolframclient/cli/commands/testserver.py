dbc982ff683fb98d0ab0e2ec22df66e7a5b23b26
Riccardo Di Virgilio <riccardod@wolfram.com>
2019-01-11 15:53:23 +0100

    adding script from other branch on an unrelated history

---
+++ wolframclient/cli/commands/testserver.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, print_function, unicode_literals
+
+from aiohttp import web
+
+from wolframclient.cli.utils import SimpleCommand
+from wolframclient.evaluation import WolframKernelPool
+from wolframclient.language import wl
+
+
+async def generate_http_response(session, request, expression):
+    if not session.started:
+        await session.start()
+    return (await session.evaluate(
+        wl.GenerateHTTPResponse(expression, request)(
+            ("BodyByteArray", "Headers", "StatusCode"))))
+
+
+class Command(SimpleCommand):
+    """ Run test suites from the tests modules.
+    A list of patterns can be provided to specify the tests to run.
+    """
+
+    port = 18000
+
+    def get_web_app(self, session):
+
+        routes = web.RouteTableDef()
+
+        @routes.get('/')
+        async def hello(request):
+            response = await generate_http_response(session, request, wl.APIFunction({"x": "String"}))
+            return web.Response(
+                body=response['BodyByteArray'],
+                status=response['StatusCode'],
+                headers=dict(rule.args for rule in response['Headers']))
+
+        app = web.Application()
+        app.add_routes(routes)
+
+        return app
+
+    def exception_handler(self, exception, context):
+        pass
+
+    def handle(self):
+
+        session = WolframKernelPool(
+            '/Applications/Mathematica.app/Contents/MacOS/WolframKernel')
+        web.run_app(self.get_web_app(session), port=self.port)
