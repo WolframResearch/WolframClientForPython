890e7c4adf268b2b565e69dc268eb9ce75d42934
Dorian Birraux <dorianb@wolfram.com>
2019-01-15 12:09:51 +0100

    Add author to main __init__
    Use author in doc file conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ project = u'Wolfram Client Library for Python'
 year = datetime.datetime.now().year
 
 copyright = u'%i, Wolfram Research' % year
-author = u'Wolfram Research'
+author = wolframclient.__author__
 
 # The short X.Y version
 # version = 'x.y'
