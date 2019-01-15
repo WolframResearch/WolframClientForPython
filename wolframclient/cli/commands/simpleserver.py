5602cabb8f1a0103f22e278eeeb77a4fe2602d62
Riccardo Di Virgilio <riccardod@wolfram.com>
2019-01-15 17:22:57 +0100

    adding aiohttp request processor

--- wolframclient/cli/commands/simpleserver.py
+++ wolframclient/cli/commands/simpleserver.py
@@ -2,59 +2,15 @@
 
 from __future__ import absolute_import, print_function, unicode_literals
 
-import os
-import shutil
-import tempfile
-import uuid
-
 from aiohttp import web
-from aiohttp.multipart import CIMultiDict
 
 from wolframclient.cli.utils import SimpleCommand
 from wolframclient.evaluation import (WolframEvaluatorPool,
                                       WolframLanguageAsyncSession)
+from wolframclient.http import aiohttp_wl_view
 from wolframclient.language import wl, wlexpr
-from wolframclient.utils import six
 from wolframclient.utils.api import asyncio
-from wolframclient.utils.decorators import to_dict
-from wolframclient.utils.encoding import force_text
 from wolframclient.utils.functional import composition, first
-
-
-def to_multipart(v):
-    if isinstance(v, six.string_types):
-        return {"ContentString": v, "InMemory": True}
-
-    destdir = os.path.join(tempfile.gettempdir(), force_text(uuid.uuid4()))
-    os.mkdir(destdir)
-
-    with open(os.path.join(destdir, v.filename), 'wb') as dest:
-        shutil.copyfileobj(v.file, dest)
-        return {
-            "FileName": dest.name,
-            "InMemory": False,
-            "OriginalFileName": v.filename
-        }
-
-
-@to_dict
-def aiohttp_request_to_response(request, post):
-    yield 'Method', request.method,
-    yield 'Scheme', request.url.scheme,
-    yield 'Domain', request.url.host,
-    yield 'Port', force_text(request.url.port),
-    yield 'PathString', request.url.path,
-    yield 'QueryString', request.url.query_string,
-    yield 'Headers', tuple(wl.Rule(k, v) for k, v in request.headers.items())
-    yield 'MultipartElements', tuple(
-        wl.Rule(k, to_multipart(v)) for k, v in post.items())
-
-
-async def generate_http_response(session, request, expression):
-    wl_req = aiohttp_request_to_response(request, await request.post())
-    return await session.evaluate(
-        wl.GenerateHTTPResponse(expression, wl_req)(("BodyByteArray",
-                                                     "Headers", "StatusCode")))
 
 
 class Command(SimpleCommand):
@@ -116,13 +72,9 @@ class Command(SimpleCommand):
         routes = web.RouteTableDef()
 
         @routes.route('*', '/{tail:.*}')
+        @aiohttp_wl_view(session)
         async def hello(request):
-            response = await generate_http_response(session, request, handler)
-            return web.Response(
-                body=response.get('BodyByteArray', b''),
-                status=response.get('StatusCode', 200),
-                headers=CIMultiDict(
-                    rule.args for rule in response.get('Headers', ())))
+            return handler
 
         app = web.Application()
         app.add_routes(routes)
---
+++ wolframclient/http/aiohttp.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, print_function, unicode_literals
+
+from aiohttp import web
+from aiohttp.multipart import CIMultiDict
+
+from wolframclient.language import wl
+from wolframclient.utils import six
+from wolframclient.utils.decorators import to_dict
+from wolframclient.utils.encoding import force_text
+
+
+def to_multipart(v):
+    if isinstance(v, six.string_types):
+        return {"ContentString": v, "InMemory": True}
+
+    destdir = os.path.join(tempfile.gettempdir(), force_text(uuid.uuid4()))
+    os.mkdir(destdir)
+
+    with open(os.path.join(destdir, v.filename), 'wb') as dest:
+        shutil.copyfileobj(v.file, dest)
+        return {
+            "FileName": dest.name,
+            "InMemory": False,
+            "OriginalFileName": v.filename
+        }
+
+
+@to_dict
+def aiohttp_request_to_response(request, post):
+    yield 'Method', request.method,
+    yield 'Scheme', request.url.scheme,
+    yield 'Domain', request.url.host,
+    yield 'Port', force_text(request.url.port),
+    yield 'PathString', request.url.path,
+    yield 'QueryString', request.url.query_string,
+    yield 'Headers', tuple(wl.Rule(k, v) for k, v in request.headers.items())
+    yield 'MultipartElements', tuple(
+        wl.Rule(k, to_multipart(v)) for k, v in post.items())
+
+
+async def generate_http_response(session, request, expression):
+    wl_req = aiohttp_request_to_response(request, await request.post())
+
+    response = await session.evaluate(
+        wl.GenerateHTTPResponse(expression, wl_req)(("BodyByteArray",
+                                                     "Headers", "StatusCode")))
+
+    return web.Response(
+        body=response.get('BodyByteArray', b''),
+        status=response.get('StatusCode', 200),
+        headers=CIMultiDict(rule.args for rule in response.get('Headers', ())))
