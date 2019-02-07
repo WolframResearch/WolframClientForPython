5bab422ceeee8514998fb8b0a40fa452ff8f4ab5
Riccardo Di Virgilio <riccardod@wolfram.com>
2019-02-07 16:09:56 +0100

    adding imports up

--- docs/conf.py
+++ docs/conf.py
@@ -8,18 +8,21 @@
 
 # -- Path setup --------------------------------------------------------------
 
+from __future__ import absolute_import, print_function, unicode_literals
+
+import datetime
+import os
+import sys
+import wolframclient
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 # import os
-import sys
-import os
+
 sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('roles'))
-
-import datetime
-import wolframclient
 
 # -- Project information -----------------------------------------------------
 
