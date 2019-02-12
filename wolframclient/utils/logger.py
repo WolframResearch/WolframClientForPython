b94fd0ceb5b2eae1c8877c270362ce7622fca062
Dorian Birraux <dorianb@wolfram.com>
2019-02-12 16:00:54 +0100

    Minor change in logger utils
    explicit named arguments

--- wolframclient/utils/logger.py
+++ wolframclient/utils/logger.py
@@ -8,20 +8,20 @@ from wolframclient.utils import six
 
 if six.PY2:
 
-    def setup_logging_to_file(path, level=logging.INFO):
+    def setup_logging_to_file(path, level=None):
         logging.basicConfig(
             filename=path,
             filemode='a',
             format='%(asctime)s, %(name)s %(levelname)s %(message)s',
-            level=level)
+            level=level or logging.INFO)
 else:
 
-    def setup_logging_to_file(path, level=logging.INFO):
+    def setup_logging_to_file(path, level=None):
         """Setup a basic Python logging configuration to a given file."""
         logging.basicConfig(
             format=u'%(asctime)s, %(name)s %(levelname)s %(message)s',
-            handlers=[logging.FileHandler(path, 'a', 'utf-8')],
-            level=level)
+            handlers=[logging.FileHandler(path, mode='a', encoding='utf-8')],
+            level=level or logging.INFO)
 
 
 def str_trim(o, max_char=80):
