6a39ef46f97cb78da02faaa741eadd6b512636d3
Dorian Birraux <dorianb@wolfram.com>
2019-02-19 14:38:04 +0100

    fix bugs in async session
    Fix coroutines not being awaited
    remove un-used code.

--- wolframclient/evaluation/kernel/asyncsession.py
+++ wolframclient/evaluation/kernel/asyncsession.py
@@ -61,26 +61,20 @@ class WolframLanguageAsyncSession(WolframLanguageSession,
             inputform_string_evaluation=inputform_string_evaluation,
             wxf_bytes_evaluation=wxf_bytes_evaluation,
             **kwargs)
-        self.thread_pool_exec = None
 
     def duplicate(self):
         return self.__class__(
-            self.kernel,
+            kernel=self.kernel,
             loop=self._loop,
             consumer=self.consumer,
             initfile=self.initfile,
-            kernel_loglevel=self.loglevel,
+            kernel_loglevel=self.kernel_loglevel,
             stdin=self._stdin,
             stdout=self._stdout,
             stderr=self._stderr,
             inputform_string_evaluation=self.inputform_string_evaluation,
             wxf_bytes_evaluation=self.wxf_bytes_evaluation,
             **self.parameters)
-
-    def _get_exec_pool(self):
-        if self.thread_pool_exec is None:
-            self.thread_pool_exec = futures.ThreadPoolExecutor(max_workers=1)
-        return self.thread_pool_exec
 
     async def do_evaluate_future(self, expr, result_update_callback=None, **kwargs):
         future = super().do_evaluate_future(expr, result_update_callback=result_update_callback, **kwargs)
@@ -110,13 +104,17 @@ class WolframLanguageAsyncSession(WolframLanguageSession,
         """Evaluate an expression and return the WXF output asynchronously.
 
         This method is a coroutine."""
-        return await self.evaluate_wxf_future(expr, **kwargs).result()
+        future = await self.evaluate_wxf_future(expr, **kwargs)
+        await future
+        return future.result()
 
     async def evaluate_wrap(self, expr, **kwargs):
         """Evaluate an expression and return a result object asynchronously.
 
         This method is a coroutine."""
-        return await self.evaluate_wrap_future(expr, **kwargs).result()
+        future = await self.evaluate_wrap_future(expr, **kwargs)
+        await future
+        return future.result()
 
     async def ensure_started(self):
         if not self.started:
@@ -133,46 +131,25 @@ class WolframLanguageAsyncSession(WolframLanguageSession,
         """Asynchronously start the session.
         
         This method is a coroutine."""
-        try:
-            await self._loop.run_in_executor(
-                self._get_exec_pool(),
-                partial(super()._start, block=True),
-                )
-        except Exception as e:
-            await self.terminate()
-            raise e
+        future = super().start_future()
+        await asyncio.wrap_future(future, loop=self._loop)
 
     async def stop(self):
         """Asynchronously stop the session (gracefully termination).
         
         This method is a coroutine."""
-        await self._async_terminate(super().stop, True)
+        await self._async_terminate(True)
 
     async def terminate(self):
         """Asynchronously terminate the session.
         
         This method is a coroutine."""
-        await self._async_terminate(super().terminate, False)
+        await self._async_terminate(False)
 
-    async def _async_terminate(self, sync_stop_func, wait):
-        # Check that the thread pool was created, otherwise it's an initialized session
-        # that was never started, and use it to asynchronously stop the kernel.
-        if self.thread_pool_exec:
-            try:
-                logger.info('Terminating asynchronous kernel session.')
-                await self._loop.run_in_executor(self._get_exec_pool(),
-                                                 sync_stop_func)
-            except asyncio.CancelledError:
-                logger.info('Cancelled terminate task.')
-            except Exception as e:
-                logger.info(
-                    'Failed to terminate kernel asynchronously: %s' % e)
-            # kernel stopped. Joining thread.
-            try:
-                self.thread_pool_exec.shutdown(wait=wait)
-                self.thread_pool_exec = None
-            except Exception as e:
-                logger.info('Thread pool executor error on shutdown: %s', e)
-        # synchronous termination required if the kernel is still not terminated.
-        if self.started:
-            sync_stop_func()
+    async def _async_terminate(self, gracefully):
+        try:
+            logger.info('Terminating asynchronous kernel session.')
+            future = super().stop_future(gracefully=gracefully)
+            await asyncio.wrap_future(future, loop=self._loop)
+        finally:
+            self.kernel_controller.join()
