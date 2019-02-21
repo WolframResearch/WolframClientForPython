09763a8a026ea71fa26908dc0fc790c4cb2a0add
Dorian Birraux <dorianb@wolfram.com>
2019-02-21 17:24:13 +0100

    Use the path module in kernel controller.

--- wolframclient/evaluation/kernel/kernelcontroller.py
+++ wolframclient/evaluation/kernel/kernelcontroller.py
@@ -11,6 +11,7 @@ from threading import Event, RLock, Thread
 
 from wolframclient.evaluation.kernel.zmqsocket import Socket, SocketException
 from wolframclient.evaluation.result import WolframKernelEvaluationResult
+from wolframclient.evaluation.kernel.path import find_default_kernel_path
 from wolframclient.exception import WolframKernelException
 from wolframclient.utils import six
 from wolframclient.utils.api import json, os, time, zmq
@@ -88,59 +89,6 @@ class KernelLogger(Thread):
                 self.socket.close()
             except:
                 logger.fatal('Failed to close ZMQ logging socket.')
-
-
-if six.WINDOWS or six.LINUX:
-    if six.WINDOWS:
-        APP_ROOT_PATH = [
-            'C:\\Program Files\\Wolfram Research\\Wolfram Desktop\\',
-            'C:\\Program Files\\Wolfram Research\\Mathematica\\',
-        ]
-        EXE_REL_PATH = 'wolfram.exe'
-    elif six.LINUX:
-        APP_ROOT_PATH = [
-            '/usr/local/Wolfram/Desktop/',
-            '/usr/local/Wolfram/Mathematica/',
-        ]
-        EXE_REL_PATH = '/Files/Executables/wolfram'
-
-    def find_default_kernel_path():
-        highest_version = -1
-        best_path = None
-        for root in APP_ROOT_PATH:
-            if os.isdir(root):
-                for version in os.listdir(root):
-                    full_path = os.path_join(root, version)
-                    if os.isdir(full_path):
-                        try:
-                            v_num = float(version)
-                        except ValueError:
-                            continue
-                        if v_num > highest_version:
-                            highest_version = v_num
-                            best_path = full_path
-        if highest_version > 0:
-            return os.path_join(best_path, EXE_REL_PATH)
-        else:
-            return None
-elif six.MACOS:
-    DEFAULT_PATHS = [
-        '/Applications/Wolfram Desktop.app/Contents/MacOS/WolframKernel',
-        '/Applications/Mathematica.app/Contents/MacOS/WolframKernel',
-    ]
-
-    def find_default_kernel_path():
-        for path in DEFAULT_PATHS:
-            if os.isfile(path):
-                return path
-        return None
-else:
-
-    def find_default_kernel_path():
-        return None
-
-
-find_default_kernel_path.__doc__ = """ Look for the most recent Wolfram Engine. """
 
 
 class WolframEngineController(Thread):
