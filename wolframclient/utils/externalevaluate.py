96571e14af32bc5f3583e33756ac3675c5187d02
Riccardo Di Virgilio <riccardod@wolfram.com>
2019-04-03 15:48:54 +0200

    Revert "fixing EvaluationContext"
    This reverts commit 501d82439271beb18df544ee1e73243450632d54.

--- wolframclient/utils/externalevaluate.py
+++ wolframclient/utils/externalevaluate.py
@@ -6,7 +6,6 @@ import ast
 import logging
 import os
 import sys
-from itertools import chain
 
 from wolframclient.deserializers import binary_deserialize
 from wolframclient.language import wl
@@ -15,13 +14,15 @@ from wolframclient.language.side_effects import side_effect_logger
 from wolframclient.serializers import export
 from wolframclient.utils import six
 from wolframclient.utils.api import zmq
+from wolframclient.utils.datastructures import Settings
 from wolframclient.utils.encoding import force_text
 from wolframclient.utils.functional import last
+from wolframclient.utils.importutils import import_string
 
 if six.PY_35:
-    pass
+    from collections.abc import Mapping
 else:
-    pass
+    from collections import Mapping
 
 HIDDEN_VARIABLES = (
     '__loader__', '__builtins__', '__traceback_hidden_variables__',
@@ -33,51 +34,14 @@ EXPORT_KWARGS = {
     'allow_external_objects': True,
 }
 
+def EvaluationContext(code, session_data = {}, context = None, **extra):
 
-class EvaluationContext(dict):
-    def __init__(self, code, context, session_data, **extra):
+    session_data['__loader__'] = Settings(get_source=lambda module, code = code: code)
+    session_data['__traceback_hidden_variables__'] = HIDDEN_VARIABLES
+    if context:
+        session_data.update(context)
 
-        self.code = code
-        self.context = context
-        self.session_data = session_data
-
-        super(EvaluationContext, self).__init__(
-            __loader__=self.get_code,
-            __traceback_hidden_variables__=HIDDEN_VARIABLES,
-        )
-
-    def get_code(self, *args, **opts):
-        return self.code
-
-    def __getitem__(self, k):
-        for d in (self, self.context, self.session_data):
-            try:
-                return dict.__getitem__(d, k)
-            except KeyError:
-                pass
-
-        raise KeyError(k)
-
-    def __setitem__(self, k, v):
-        for d in (self.context, self.session_data):
-            dict.__setitem__(d, k, v)
-
-    def __iter__(self):
-        return iter(self.keys())
-
-    def keys(self):
-        return frozenset(
-            chain.from_iterable(
-                dict.keys(d) for d in (self, self.context, self.session_data)))
-
-    def values(self):
-        return (self[k] for k in self)
-
-    def items(self):
-        return ((k, self[k]) for k in self)
-
-    def __repr__(self):
-        return '<%s>' % self.__class__.__name__
+    return session_data
 
 
 def execute_from_file(path, *args, **opts):
@@ -85,20 +49,19 @@ def execute_from_file(path, *args, **opts):
         return execute_from_string(force_text(f.read()), *args, **opts)
 
 
-def execute_from_string(code, globals={}, context={}, session_data={}, **opts):
+def execute_from_string(code, globals = {}, **opts):
 
     __traceback_hidden_variables__ = True
 
     #this is creating a custom __loader__ that is returning the source code
     #traceback serializers is inspecting global variables and looking for a standard loader that can return source code.
 
-    context = EvaluationContext(
-        code, context=context, session_data=session_data)
-    result = None
+    context     = EvaluationContext(code = code, **opts)
+    result      = None
     expressions = list(ast.parse(code).body)
 
     if not expressions:
-        return
+        return 
 
     if isinstance(last(expressions), ast.Expr):
         result = expressions.pop(-1)
@@ -114,7 +77,6 @@ class SideEffectSender(logging.Handler):
     def emit(self, record):
         if isinstance(sys.stdout, StdoutProxy):
             sys.stdout.send_side_effect(record.msg)
-
 
 class SocketWriter:
     def __init__(self, socket):
@@ -166,7 +128,10 @@ class StdoutProxy:
         self.stream.write(export(self.keep_listening(expr), **EXPORT_KWARGS))
 
 
-def evaluate_message(input=None, return_type=None, args=None, **opts):
+def evaluate_message(input=None,
+                     return_type=None,
+                     args=None,
+                     **opts):
 
     __traceback_hidden_variables__ = True
 
@@ -191,7 +156,7 @@ def handle_message(socket):
     __traceback_hidden_variables__ = True
 
     message = binary_deserialize(socket.recv())
-    result = evaluate_message(**message)
+    result  = evaluate_message(**message)
 
     sys.stdout.flush()
     return result
@@ -224,7 +189,7 @@ def start_zmq_loop(message_limit=float('inf'), redirect_stdout=True, **opts):
 
     if redirect_stdout:
         sys.stdout = StdoutProxy(stream)
-
+    
     side_effect_logger.addHandler(SideEffectSender())
 
     #now sit in a while loop, evaluating input
