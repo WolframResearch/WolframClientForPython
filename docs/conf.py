2091ba8767d0b9abf0165cd1136ba05d9dd83b9e
Yang Yang <wdscxsj@gmail.com>
2019-05-17 10:29:51 +0800

    Fix import error of extension module "roles"

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ import wolframclient
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 
 sys.path.insert(0, os.path.abspath('..'))
-sys.path.insert(0, os.path.abspath('roles'))
+sys.path.insert(0, os.path.abspath('.'))
 
 # -- Project information -----------------------------------------------------
 
