5cdfa2952ae16c0594658b3e6b109d554b8b8e08
Riccardo Di Virgilio <riccardod@wolfram.com>
2019-05-23 14:45:02 +0200

    adding back isort with line reorder

--- wolframclient/cli/commands/refactor.py
+++ wolframclient/cli/commands/refactor.py
@@ -12,7 +12,7 @@ class Command(SimpleCommand):
 
     modules = ['wolframclient']
 
-    dependencies = (('autoflake', '1.2'), ('black', '19.3b0'))
+    dependencies = (('isort', '4.3.4'), ("autoflake", "1.2"), ("black", "19.3b0"))
 
     def _module_args(self, *args):
 
@@ -39,8 +39,18 @@ class Command(SimpleCommand):
 
         from black import main
 
-        sys.argv = list(self._module_args("--line-length", "95"))
+        sys.argv = list(self._module_args("--line-length", "95", "--target-version", "py35"))
 
         main()
 
         sys.argv = argv
+
+        from isort.main import main
+
+        sys.argv = list(
+            self._module_args(
+                '-rc', '--multi-line', '5', '-a',
+                "from __future__ import absolute_import, print_function, unicode_literals"
+            ))
+
+        main()
