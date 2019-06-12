b8471101c2dc0ca4bd128f3b93caebcb6256987e
Riccardo Di Virgilio <riccardod@wolfram.com>
2019-06-12 17:48:26 +0200

    using class method

--- wolframclient/cli/commands/refactor.py
+++ wolframclient/cli/commands/refactor.py
@@ -8,13 +8,6 @@ from wolframclient.cli.utils import SimpleCommand
 from wolframclient.utils.importutils import module_path, safe_import_string_and_call
 
 
-def run(path, args):
-    originals = sys.argv
-    sys.argv = list(args)
-    safe_import_string_and_call(path)
-    sys.argv = originals
-
-
 class Command(SimpleCommand):
 
     modules = ["wolframclient"]
@@ -31,38 +24,40 @@ class Command(SimpleCommand):
         for arg in args:
             yield arg
 
+    def run(self, path, *args):
+        originals = sys.argv
+        sys.argv = list(self._module_args(*args))
+        safe_import_string_and_call(path)
+        sys.argv = originals
+
     def handle(self, **opts):
 
         # autoflake is not removing imports if they are in a list
         # to fix this we first refactor the code using imports in single line
 
-        run(
+        self.run(
             "isort.main.main",
-            self._module_args(
-                "-rc",
-                "-sl",
-                "-a",
-                "from __future__ import absolute_import, print_function, unicode_literals",
-            ),
+            "-rc",
+            "-sl",
+            "-a",
+            "from __future__ import absolute_import, print_function, unicode_literals",
         )
 
         # then we remove all missing imports and we expand star imports
 
-        run(
+        self.run(
             "autoflake.main",
-            self._module_args(
-                "--in-place",
-                "--remove-duplicate-keys",
-                "--expand-star-import",
-                "--remove-all-unused-imports",
-                "--recursive",
-            ),
+            "--in-place",
+            "--remove-duplicate-keys",
+            "--expand-star-import",
+            "--remove-all-unused-imports",
+            "--recursive",
         )
 
         # then we use refactor imports again using pretty newline style
 
-        run("isort.main.main", self._module_args("-rc", "--multi-line", "5"))
+        self.run("isort.main.main", "-rc", "--multi-line", "5")
 
         # after that we finally run black to refactor all code
 
-        run("black.main", self._module_args("--line-length", "95", "--target-version", "py34"))
+        self.run("black.main", "--line-length", "95", "--target-version", "py34")
