e8a992a056e7b0ec5b47bbb7ee4d305885712714
Riccardo Di Virgilio <riccardod@wolfram.com>
2019-06-12 16:12:55 +0200

    using module_path

--- wolframclient/utils/tests.py
+++ wolframclient/utils/tests.py
@@ -2,17 +2,14 @@
 
 from __future__ import absolute_import, print_function, unicode_literals
 
-import os
 import unittest
+
+from wolframclient.utils.importutils import module_path
 
 
 class TestCase(unittest.TestCase):
     pass
 
 
-HERE = os.path.dirname(__file__)
-DATA_DIR = os.path.abspath(os.path.join(HERE, "..", "tests", "data"))
-
-
-def path_to_file_in_data_dir(file_name):
-    return os.path.join(DATA_DIR, file_name)
+def path_to_file_in_data_dir(*args):
+    return module_path("wolframclient", "tests", "data", *args)
