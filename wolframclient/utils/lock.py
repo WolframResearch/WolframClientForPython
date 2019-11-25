3a5127089d5b862685a0c4efeaf4379114dcfd33
Riccardo Di Virgilio <riccardod@wolfram.com>
2019-11-25 16:32:26 +0100

    there is no need for context manager

--- wolframclient/utils/lock.py
+++ wolframclient/utils/lock.py
@@ -1,22 +1,20 @@
 from __future__ import absolute_import, print_function, unicode_literals
 
-from contextlib import contextmanager
-
 try:
     import multithreading
 
     _lock = multithreading.Lock()
 
-    @contextmanager
     def Lock():
-        with _lock:
-            yield
+        return _lock
 
 
 except (ImportError, OSError):
 
-    #JYTHON is raising an ImportError when running "import multithreading"
-    #GVisor is raising an OSError when running "multithreading.Lock()" because the feature is not implemented
+    # JYTHON is raising an ImportError when running "import multithreading"
+    # GVisor is raising an OSError when running "multithreading.Lock()" because the feature is not implemented
+
+    from contextlib import contextmanager
 
     @contextmanager
     def Lock():
