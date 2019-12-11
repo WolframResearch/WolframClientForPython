6ca576ebb7c03a64e9f12c1f7c1b38ac6b75ab83
Dorian Birraux <dorianb@wolfram.com>
2019-12-11 15:52:04 +0100

    multithreading module does not exist
    replacing it by multiprocessing

--- wolframclient/utils/lock.py
+++ wolframclient/utils/lock.py
@@ -3,9 +3,9 @@ from __future__ import absolute_import, print_function, unicode_literals
 import warnings
 
 try:
-    import multithreading
+    import multiprocessing
 
-    _lock = multithreading.Lock()
+    _lock = multiprocessing.Lock()
 
     def Lock():
         return _lock
@@ -13,8 +13,8 @@ try:
 
 except (ImportError, OSError):
 
-    # JYTHON is raising an ImportError when running "import multithreading"
-    # GVisor is raising an OSError when running "multithreading.Lock()" because the feature is not implemented
+    # JYTHON is raising an ImportError when running "import multiprocessing"
+    # GVisor is raising an OSError when running "multiprocessing.Lock()" because the feature is not implemented
 
     from contextlib import contextmanager
 
