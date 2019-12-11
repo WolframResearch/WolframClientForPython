ce6104891ed45de68626156d8936ac387117a97e
Dorian Birraux <dorianb@wolfram.com>
2019-12-11 15:52:04 +0100

    Update lock.py

--- wolframclient/utils/lock.py
+++ wolframclient/utils/lock.py
@@ -3,15 +3,14 @@ from __future__ import absolute_import, print_function, unicode_literals
 from wolframclient.utils.importutils import safe_import_string_and_call
 
 try:
-
     _lock = safe_import_string_and_call('multithreading.Lock')
     def Lock():
         return _lock
 
 except (ImportError, OSError):
 
-    # JYTHON is raising an ImportError when running "import multithreading"
-    # GVisor is raising an OSError when running "multithreading.Lock()" because the feature is not implemented
+    # JYTHON is raising an ImportError when running "import multiprocessing"
+    # GVisor is raising an OSError when running "multiprocessing.Lock()" because the feature is not implemented
 
     from contextlib import contextmanager
     import warnings
