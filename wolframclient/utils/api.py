37c45ea8e7174dc68137d0e5a8def353a545c867
Riccardo Di Virgilio <riccardod@wolfram.com>
2019-12-16 17:32:03 +0100

    adding ast api

--- wolframclient/utils/api.py
+++ wolframclient/utils/api.py
@@ -2,6 +2,15 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 from wolframclient.utils.importutils import API
 
+ast = API(
+    Module = 'ast.Module',
+    PyCF_ONLY_AST = 'ast.PyCF_ONLY_AST',
+    Expr = 'ast.Expr',
+    Expression = 'ast.Expression',
+    FunctionDef = 'ast.FunctionDef',
+    ClassDef = 'ast.ClassDef',
+)
+
 pytz = API(
     FixedOffset="pytz.FixedOffset",
     timezone="pytz.timezone",
