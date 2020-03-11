6be628412fb1f5433c266b1db7dcf3502c44faa1
Riccardo Di Virgilio <riccardod@wolfram.com>
2020-03-11 16:08:32 +0100

    testing shape

--- wolframclient/tests/serializers/wxf_pythonarray.py
+++ wolframclient/tests/serializers/wxf_pythonarray.py
@@ -9,17 +9,24 @@ from wolframclient.utils.tests import TestCase as BaseTestCase
 
 
 class TestCase(BaseTestCase):
+    
     def test_python_array(self):
 
         for array, numpy_type, wl_type in (
-            ([1, 2, 3], numpy.int8, "Integer8"),
-            ([1, 2, 3], numpy.int32, "Integer32"),
-            ([1, 2, 3], numpy.int64, "Integer64"),
-            ([1.2, 2.3, 3], numpy.float32, "Real32"),
-            ([1.2, 2.3, 3], numpy.float64, "Real64"),
+            ([1, 2, 3, 4, 5, 6], numpy.int8, "Integer8"),
+            ([1, 2, 3, 4, 5, 6], numpy.int32, "Integer32"),
+            ([1, 2, 3, 4, 5, 6], numpy.int64, "Integer64"),
+            ([1.2, 2.3, 3, 4, 5, 6], numpy.float32, "Real32"),
+            ([1.2, 2.3, 3, 4, 5, 6], numpy.float64, "Real64"),
         ):
 
-            self.assertEqual(
-                export(numpy.array(array, numpy_type), target_format="wxf"),
-                export(PythonArray(array, wl_type), target_format="wxf"),
-            )
+            for shape in ((3, 2), None):
+
+                arr = numpy.array(array, numpy_type)
+                if shape:
+                    arr = arr.reshape(shape)
+
+                self.assertEqual(
+                    export(arr, target_format="wxf"),
+                    export(PythonArray(array, wl_type, shape = shape), target_format="wxf"),
+                )
