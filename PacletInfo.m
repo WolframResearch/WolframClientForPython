689f758264377b86c66bf34c4c137bccd9aa32ef
Riccardo Di Virgilio <riccardod@wolfram.com>
2021-03-31 10:51:54 -0500

    PacletInfo.m edited online with Bitbucket

--- PacletInfo.m
+++ PacletInfo.m
@@ -1,7 +1,7 @@
 Paclet[
     Name -> "WolframClientForPython",
     Version -> "1.1.4",
-    MathematicaVersion -> "12.2+",
+    MathematicaVersion -> "12.3+",
     Loading -> Automatic,
     Extensions -> {}
 ]
