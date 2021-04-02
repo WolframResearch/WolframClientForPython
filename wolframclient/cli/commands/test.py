11902d362e23e143e3e979ba67e2cc647fa7a202
Riccardo Di Virgilio <riccardod@wolfram.com>
2021-04-02 14:02:13 +0200

    moving dependency install to a setup command

--- wolframclient/cli/commands/test.py
+++ wolframclient/cli/commands/test.py
@@ -9,23 +9,6 @@ from wolframclient.utils.decorators import to_tuple
 from wolframclient.utils.functional import map
 from wolframclient.utils.importutils import module_path
 
-
-# @to_tuple
-# def dependencies():
-    # yield ("pytz", "2018.6")
-    #
-    # if not six.JYTHON:
-    #     yield ("numpy", not six.PY2 and "1.15.3" or None)
-    #     yield ("pillow", "7.1.2")
-    #     yield ("requests", "2.20.0")
-    #     yield ("oauthlib", "2.1.0")
-    #     yield ("pyzmq", "17.1.2")
-    #     yield ("pandas", "1.0.4")
-    #     yield ("unittest-xml-reporting", None)
-    # if not six.PY2:
-    #     yield ("aiohttp", "3.6.2")
-
-
 class Command(SimpleCommand):
     """ Run test suites from the tests modules.
     A list of patterns can be provided to specify the tests to run.
@@ -33,7 +16,6 @@ class Command(SimpleCommand):
 
     modules = ["wolframclient.tests"]
 
-    dependencies = ()
 
     def add_arguments(self, parser):
         parser.add_argument(
