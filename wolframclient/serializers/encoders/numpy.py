bfe760c611f3974bcae60f694fee1038fb33983f
riccardodivirgilio <riccardodivirgilio@gmail.com>
2024-03-11 15:08:25 +0100

    code unification between packed array and numeric array

--- wolframclient/serializers/encoders/numpy.py
+++ wolframclient/serializers/encoders/numpy.py
@@ -4,25 +4,26 @@ import sys
 
 from wolframclient.utils.api import numpy
 from wolframclient.utils.dispatch import Dispatch
-from wolframclient.utils.functional import map
+from wolframclient.utils.functional import map, first
 
 encoder = Dispatch()
 
-NUMPY_MAPPING = {
-    numpy.int8: "Integer8",
-    numpy.int16: "Integer16",
-    numpy.int32: "Integer32",
-    numpy.int64: "Integer64",
-    numpy.uint8: "UnsignedInteger8",
-    numpy.uint16: "UnsignedInteger16",
-    numpy.uint32: "UnsignedInteger32",
-    numpy.uint64: "UnsignedInteger64",
-    numpy.float32: "Real32",
-    numpy.float64: "Real64",
-    numpy.complex64: "ComplexReal32",
-    numpy.complex128: "ComplexReal64",
-}
 
+
+NUMPY_MAPPING = {
+    numpy.int8: ("Integer8", None),
+    numpy.int16: ("Integer16", None),
+    numpy.int32: ("Integer32", None),
+    numpy.int64: ("Integer64", None),
+    numpy.uint8: ("Integer16", numpy.int16),
+    numpy.uint16: ("Integer32", numpy.int32),
+    numpy.uint32: ("Integer64", numpy.int64),
+    # numpy.uint64: ("UnsignedInteger64", None),
+    numpy.float32: ("Real32", None),
+    numpy.float64: ("Real64", None),
+    numpy.complex64: ("ComplexReal32", None),
+    numpy.complex128: ("ComplexReal64", None),
+}
 
 SYS_IS_LE = sys.byteorder == "little"
 
@@ -44,15 +45,20 @@ def encode_ndarray(serializer, o):
     if not o.shape:
         return serializer.encode(o.item())
 
+    is_packed_array = isinstance(o, numpy.PackedArray)
+
     try:
-        wl_type = NUMPY_MAPPING[o.dtype.type]
+        wl_type, cast_to = NUMPY_MAPPING[o.dtype.type]
     except KeyError:
         raise NotImplementedError(
-            "NumPy serialization not implemented for %s. Choices are: %s"
+            "Numpy serialization not implemented for %s. Choices are: %s"
             % (repr(o.dtype), ", ".join(map(repr, NUMPY_MAPPING.keys())))
         )
-
     o = to_little_endian(o)
+
+
+    if is_packed_array and cast_to is not None:
+        o = o.astype(cast_to)
 
     if hasattr(o, "tobytes"):
         # Numpy 1.9+ support array.tobytes, but previous versions don't and use tostring instead.
@@ -60,47 +66,13 @@ def encode_ndarray(serializer, o):
     else:
         data = o.tostring()
 
-    return serializer.serialize_numeric_array(data, o.shape, wl_type)
-
-
-PACKEDARRAY_NUMPY_MAPPING = {
-    numpy.int8: ("Integer8", None),
-    numpy.int16: ("Integer16", None),
-    numpy.int32: ("Integer32", None),
-    numpy.int64: ("Integer64", None),
-    numpy.uint8: ("Integer16", numpy.int16),
-    numpy.uint16: ("Integer32", numpy.int32),
-    numpy.uint32: ("Integer64", numpy.int64),
-    # numpy.uint64: ("UnsignedInteger64", None),
-    numpy.float32: ("Real32", None),
-    numpy.float64: ("Real64", None),
-    numpy.complex64: ("ComplexReal32", None),
-    numpy.complex128: ("ComplexReal64", None),
-}
-"""
-Maps numpy dtype to appropriate wxf packed array type, eventually specifying the type to cast the data to.
-"""
-
-
-@encoder.dispatch(numpy.PackedArray)
-def encode_packed_array(serializer, o):
-    try:
-        wl_type, cast_to = PACKEDARRAY_NUMPY_MAPPING[o.dtype.type]
-    except KeyError:
-        raise NotImplementedError(
-            "Packed array serialization not implemented for %s. Choices are: %s"
-            % (repr(o.dtype), ", ".join(map(repr, PACKEDARRAY_NUMPY_MAPPING.keys())))
-        )
-    o = to_little_endian(o)
-    if cast_to is not None:
-        o = numpy.array(o, dtype=cast_to)
-    if hasattr(o, "tobytes"):
-        # Numpy 1.9+ support array.tobytes, but previous versions don't and use tostring instead.
-        data = o.tobytes()
+    if is_packed_array:
+        return serializer.serialize_packed_array(data, o.shape, wl_type)
     else:
-        data = o.tostring()
+        return serializer.serialize_numeric_array(data, o.shape, wl_type)
 
-    return serializer.serialize_packed_array(data, o.shape, wl_type)
+
+
 
 
 @encoder.dispatch(numpy.integer)
