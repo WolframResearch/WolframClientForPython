d1fb25ba157a727bfa52d2d596d381f7d2a2e70c
riccardodivirgilio <riccardodivirgilio@gmail.com>
2024-04-17 11:51:35 +0200

    bump version

--- wolframclient/about.py
+++ wolframclient/about.py
@@ -2,6 +2,6 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 __name__ = "wolframclient"
 __description__ = "A Python library with various tools to interact with the Wolfram Language and the Wolfram Cloud."
-__version__ = "1.2.0"
+__version__ = "1.2.1"
 __author__ = "Wolfram Research"
 __author_email__ = "support@wolfram.com, dorianb@wolfram.com, riccardod@wolfram.com"
